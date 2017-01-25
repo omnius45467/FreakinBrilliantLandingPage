1439981df4a912ef6d9d1356c518e759f05d65ee
Jeremy Robles <jeremy.robles56@gmail.com>
2017-01-25 00:27:25 -0800

    updated gulpfile and build index

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var elixir = require('laravel-elixir');
 var ghPages = require('gulp-gh-pages');
+	
+elixir(function(mix) {
+
+	mix.sass([
+		'./src/sass/app.scss', 
+		'./node_modules/materialize-css/sass/ghpages-materialize.scss'
+		], './dist/main.css');
+
+	mix.scripts([
+		
+		], './dist/main.js');
+
+	mix.copy('./src/index.html', './dist/index.html');
+
+});
  
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
