a4823208a51c8befb3ac20f4776c3011b413b39a
Jeremy Robles <jeremy.robles56@gmail.com>
2017-01-25 15:59:51 -0800

    pushing update to stylesheets and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,24 @@ var ghPages = require('gulp-gh-pages');
 	
 elixir(function(mix) {
 
-	mix.sass([
-		'./src/sass/app.scss', 
-		'./node_modules/materialize-css/sass/ghpages-materialize.scss'
-		], './dist/main.css');
-
-	mix.scripts([
-		
-		], './dist/main.js');
-
+	mix.styles([
+		'./node_modules/materialize-css/dist/css/materialize.min.css',
+		// './node_modules/materialize-css/sass/ghpages-materialize.scss',
+		'./src/sass/app.scss'
+	], './src/main.css');
+    //
+	// mix.scripts([
+	// 	'./node_modules/jquery/dist/jquery.min.js',
+	// 	'./node_modules/materialize-css/dist/js/materialize.min.js'
+	// ], './src/main.js');
+	//
 	mix.copy('./src/index.html', './dist/index.html');
+	mix.copy('./src/img', './dist/img');
+	mix.copy('./src/main.css', './dist/main.css');
+	mix.copy('./src/main.css.map', './dist/main.css.map');
+	mix.copy('./node_modules/materialize-css/fonts/roboto', './fonts/roboto');
+	// mix.copy('./src/main.js', './dist/main.js');
+	// mix.copy('./src/main.js.map', './dist/main.js.map');
 
 });
  
