373d3ac9853035b2f9ffa899f66829a6615c3331
Jeremy Robles <jeremy.robles56@gmail.com>
2017-01-25 20:39:28 -0800

    updating landing page with twitter feed and fixing deps

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,23 @@ elixir(function(mix) {
 
 	mix.styles([
 		'./node_modules/materialize-css/dist/css/materialize.min.css',
-		// './node_modules/materialize-css/sass/ghpages-materialize.scss',
 		'./src/sass/app.scss'
 	], './src/main.css');
     //
-	// mix.scripts([
-	// 	'./node_modules/jquery/dist/jquery.min.js',
-	// 	'./node_modules/materialize-css/dist/js/materialize.min.js'
-	// ], './src/main.js');
+	mix.scripts([
+		// './node_modules/jquery/dist/jquery.min.js',
+		// './node_modules/materialize-css/dist/js/materialize.min.js',
+		// './node_modules/typed/src/typed.js',
+		'./src/js/app.js'
+	], './src/main.js');
 	//
 	mix.copy('./src/index.html', './dist/index.html');
 	mix.copy('./src/img', './dist/img');
 	mix.copy('./src/main.css', './dist/main.css');
 	mix.copy('./src/main.css.map', './dist/main.css.map');
 	mix.copy('./node_modules/materialize-css/fonts/roboto', './fonts/roboto');
-	// mix.copy('./src/main.js', './dist/main.js');
-	// mix.copy('./src/main.js.map', './dist/main.js.map');
+	mix.copy('./src/main.js', './dist/main.js');
+	mix.copy('./src/main.js.map', './dist/main.js.map');
 
 });
  
